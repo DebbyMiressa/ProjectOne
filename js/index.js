4809ce952f7cee24216e39f6dc476e1c0cd38dd5
Debby Miressa <debmirmij@gmail.com>
2022-03-19 12:25:14 +0300

    Completed Project (16/16)

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 "use strict";
 
-function setId (){
-    document.getElementsByClassName("main-section")[0].id = document.getElementsByClassName("main-section").innerHTML.split(' ').join('_');
-}
+// function setId (){
+//     document.getElementsByClassName("main-section")[0].id = document.getElementsByClassName("main-section").innerHTML.split(' ').join('_');
+// }
